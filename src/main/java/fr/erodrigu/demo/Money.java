2794ce0ce3cd867a40312d99ddbc6b619ca2b27f
e_rodrigues <erodrigu@enssat.fr>
2023-09-19 17:53:25 +0200

    Added unit test for add

--- src/main/java/fr/erodrigu/demo/Money.java
+++ src/main/java/fr/erodrigu/demo/Money.java
@@ -1,5 +1,7 @@
 package fr.erodrigu.demo;
 
+import java.util.Objects;
+
 public class Money {
     int amount;
     String currency;
@@ -14,8 +16,25 @@ public class Money {
         this.currency = currency;
     }
 
+    public Money add(Money moneyToAdd){
+        return new Money(amount+=moneyToAdd.amount,this.currency);
+    }
+
     @Override
     public String toString() {
         return amount+currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount && Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
     }
 }
